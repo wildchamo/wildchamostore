0ff805af58cc43464dbcd5ea3a92ee1e7235f46a
Jose Luis <wildchamo@gmail.com>
2023-05-08 18:11:01 -0500

    base de todos los componentes

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,18 @@
+import Home from "../Home/";
+import MyAccount from "../MyAccount/";
+import MyOrders from "../MyOrders/";
+import MyOrder from "../MyOrder/";
+import NotFound from "../NotFound/";
+import SignIn from "../SignIn/";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-100">Hola, soy Jose</div>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
