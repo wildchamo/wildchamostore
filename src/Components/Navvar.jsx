70531892dc647b2d8005d84a2d1e64e4aecaa827
Jose Luis <wildchamo@gmail.com>
2023-05-08 18:51:50 -0500

    creación del componente layout para mejorar las vistas

--- src/Components/Navvar.jsx
+++ src/Components/Navvar.jsx
@@ -5,7 +5,7 @@ function Navvar() {
   let activeStyle = "underline underline-offset-4";
 
   return (
-    <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-md font-light">
+    <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-md font-light">
       <ul className="flex items-center gap-3">
         <li className="font-bold">
           <NavLink to="/">Wildchamo Store</NavLink>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Layout from "../../Components/Layout";
 
 function Home() {
   return (
-    <>
-    </>
+   <Layout>
+    Hola
+   </Layout>
   );
 }
 
