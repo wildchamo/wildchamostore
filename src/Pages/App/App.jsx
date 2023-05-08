c7311b5313b2d6711f66d25a6bd5ffa57db49e83
Jose Luis <wildchamo@gmail.com>
2023-05-08 18:19:24 -0500

    rounter listo!

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
 import Home from "../Home/";
 import MyAccount from "../MyAccount/";
 import MyOrders from "../MyOrders/";
@@ -6,12 +7,41 @@ import NotFound from "../NotFound/";
 import SignIn from "../SignIn/";
 import "./App.css";
 
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/my-account",
+      element: <MyAccount />,
+    },
+    {
+      path: "/my-orders",
+      element: <MyOrders />,
+    },
+    {
+      path: "/my-order",
+      element: <MyOrder />,
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+  return routes;
+};
+
 function App() {
   return (
-    <>
-      <div className="bg-red-100">Hola, soy Jose</div>
-      <Home></Home>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
