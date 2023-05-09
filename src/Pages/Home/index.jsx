0ae42a74068ae266fc89bbe4c481c41adff7408e
Jose Luis <wildchamo@gmail.com>
2023-05-08 19:27:50 -0500

    llamada a la api!

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,22 @@
-import React from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
+import { useState, useEffect } from "react";
 
 function Home() {
+  const [items, setItems] = useState(null);
+  console.log(items);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
   return (
-   <Layout>
-    <Card></Card>
-   </Layout>
+    <Layout>
+      {items?.map((item) => (
+        <Card key={item.id} />
+      ))}
+    </Layout>
   );
 }
 
